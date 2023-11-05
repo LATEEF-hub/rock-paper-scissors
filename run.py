b747572131d0f4ca9f9caf59c2d8cd593b853075
LATEEF-hub <latifkk@krok.edu.ua>
2023-11-05 09:17:48 +0000

    Add ASCII for game name

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
-print("Welcome to ROCK.PAPER.SCISSORS GAME ARENA")
+import pyfiglet
+
+
+playerchoice = int(input())
+
+ascii = "ROCK PAPER SICCORS"
+ascii_art= pyfiglet.figlet_format(ascii)
+print(ascii_art)
+
+

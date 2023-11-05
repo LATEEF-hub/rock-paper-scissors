29d41fdb21679be0acd27dcccd12b3052b64cce7
LATEEF-hub <latifkk@krok.edu.ua>
2023-11-05 10:41:30 +0000

    wrap welcome message in a function

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 import pyfiglet
-
-ascii = "R.P.S.L"
-ascii_art= pyfiglet.figlet_format(ascii)
-print(ascii_art)
+import pprint
 
 
+def start_message():
 
+    ascii = "ROCK\nPAPER\nSCISSORS\nLIZARD SPOCK"
+    ascii_art = pyfiglet.figlet_format(ascii.ljust(10, ".").rjust(12, "+"))
+    print(ascii_art)
+    
+start_message()

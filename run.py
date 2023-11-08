13c678692bca8bd1f797257bfff815d5b0d85bb8
LATEEF-hub <latifkk@krok.edu.ua>
2023-11-08 17:57:49 +0000

    create function for welcome page and update rule

--- run.py
+++ run.py
@@ -4,19 +4,7 @@ import pprint
 import os
 from time import sleep
 import heading_art
-
 from colorama import init, Fore, Back, Style
-
-
-def rules():
-    """
-    This function displays the game's rules.
-    """
-    print(heading_art.logo)
-    print(f'\033[2J')
-    line_break()
-    print(Fore.BLUE + "HOW TO PLAY THE AMAZING RPS GAME\033[39m")
-
 
 
 def line_break():
@@ -30,9 +18,8 @@ def clear():
     """
     Clear the screen
     """
-
+    sleep(4)
     os.system('clear')
-    sleep(5)
 
 
 def print_logo():
@@ -40,6 +27,62 @@ def print_logo():
     To print logo
     """
     print(heading_art.logo)
+
+
+def rules():
+    """
+    This function displays the game's rules.
+    """
+
+    sleep(3)
+    clear()
+    print(f'\033[2J')
+    print(heading_art.logo)
+    line_break()
+    print(Fore.MAGENTA + "HOW TO PLAY THE AMAZING RPS GAME\033[39m")
+    line_break()
+    print("Lorem ipsimhhdbfb hidsb hd cbhd cbhs dchhd dh dhhds d dhhs d bshjjhd dhhs dfshb d . dhhs dhhsbbvf d")
+    line_break()
+    input(Fore.BLUE + "Press Enter to continue...\033[39m")
+
+
+def welcome():
+    """
+    Function to display welcome message and Game options for player.
+    """
+
+    # clear()
+    sleep(5)
+    print_logo()
+    sleep(5)
+    print("Welcome to Rock, Paper, Scissors, Lizard, Spock Game\n\tYou have the following options:\n")
+    print("\t1) Start Game\n\t2) Rules of the Game \n\t3) Exit Game")
+    choice = int(
+        input(Fore.BLUE + "Select your option between 1, 2 and 3:\n"))
+    if choice == 1:
+        print(Fore.GREEN+"Starting RPS Game...")
+        sleep(2)
+        start_game()
+    elif choice == 2:
+        print("Opening the Rules Book...")
+        clear()
+        rules()
+    elif choice == 3:
+        print(Fore.WHITE + "\nExiting The Game...")
+        clear()
+        print("Click on Run Program above to restart again!")
+
+    else:
+        print(Fore.RED + "ERROR!\033[39m Invalid option please "
+              "select 1, 2 or 3\n ")
+        input(Fore.BLUE + "Press Enter to continue...\033[39m")
+
+
+def main():
+    welcome()
+
+
+main()
 
 
 # def welcome():
@@ -72,7 +115,6 @@ def print_logo():
 #                 input(Fore.BLUE + "Press Enter to continue...\033[39m")
 
 
-
 # def rules():
 #     """
 
@@ -87,13 +129,6 @@ def print_logo():
 #         print(Fore.RED + "ERROR!\033[39m Invalid option please "
 #               "select 1, 2, 3 or 4\n ")
 #         input(Fore.BLUE + "Press Enter to continue...\033[39m")
-
-
-def main():
-    welcome()
-
-
-main()
 
 
 # def play_again():
